83d65ded771867b69abab5b16bc5751af7d9d715
Sash <mr.shaljyan@gmail.com>
2021-12-12 23:24:10 +0400

    disney

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Header from './components/Header'
+import Home from './components/Home'
+import Login from './components/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Header />
+      <Switch>
+      <Route path="/login">
+      <Login />
+      </Route>
+      <Route path="/">
+      <Home />
+      </Route>
+      </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
